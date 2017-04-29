d8998a00f74049d2e1f08f44ead0022f1a87d3e9
Grig0 <marian.grigoras@fenrir.info.uaic.ro>
2017-04-29 08:58:17 +0300

    COLIW: Add ajax for calling aplication after inserting a command

--- skin/js/coliw.js
+++ skin/js/coliw.js
@@ -1,5 +1,7 @@
 
 
+
+
 $(function() {
      $('#command-window').on('keypress', function (e) {
          if(e.which === 13){
@@ -9,7 +11,17 @@ $(function() {
             //alert('enter key pressed.');
             var content = $(this).val();
             var lastLine = content.substr(content.lastIndexOf("\n")+1);
-
+            $.ajax({
+                url: '/processCommand',
+                data: {'command':lastLine},
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
          }
    });
 });
